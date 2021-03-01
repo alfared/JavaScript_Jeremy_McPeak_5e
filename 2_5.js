8f771085dee1991777b41719386d3961ff1d222b
Emil <isijafar@gmail.com>
2021-03-01 18:16:20 +0100

    1st js file

---
+++ 2_5.js
@@ -0,0 +1,7 @@
+var greetingString = "Hello";
+    var myName = prompt("Please enter your name", "");
+    var concatString;
+
+    document.write(greetingString + " " + myName + "<br/>");
+    concatString = greetingString + " " + myName;
+    document.write(concatString);
