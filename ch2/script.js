eeaf68a3f8a81f0eb438684b1d18164d71795581
vholovka <vitaliy.holovka@forevercompanies.com>
2021-03-02 16:46:38 +0100

    test commit

---
+++ ch2/script.js
@@ -0,0 +1,45 @@
+// ch2_example1.html
+var string1 = "Hello";
+var string2 = "Goodbye";
+
+alert(string1);
+alert(string2);
+
+string2 = string1;
+
+alert(string1);
+alert(string2);
+
+string1 = "Now for something different";
+
+alert(string1);
+alert(string2);
+
+// ch2_example2.html
+var string1 = "Hello";
+var string2 = "Goodbye";
+
+alert(string1);
+alert(string2);
+
+string2 = string1;
+
+alert(string1);
+alert(string2);
+
+string1 = "Now for something different";
+
+alert(string1);
+alert(string2);
+
+// ch2_example3.html
+var firstNumber = 15;
+var secondNumber = 10;
+var answer;
+answer = 15/10;
+alert(answer);
+
+alert(15/10);
+
+answer = firstNumber / secondNumber;
+alert(answer);
